95886441a03f13a1d345bd8c5e83edebb37f1dd8
Elias Pamfilis <eliasp05@icloud.com>
2025-11-21 17:24:35 -0500

    added some temporarydata into db for friends and implemented get for friends when user id is 1

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
-from .db import init_db
+from fastapi import FastAPI, Depends
+from sqlmodel import Session, select
+from .db import init_db, get_session
+from .models import Friend, FriendRead
 
 # FastAPI
 app = FastAPI()
@@ -13,3 +15,16 @@ def on_startup():
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
+
+# Friends API:
+@app.get("/friends", response_model=list[FriendRead])
+def get_friends(session: Session = Depends(get_session)):
+    # For now, we assume the current student has user id 1
+    owner_user_id = 1
+
+    friends = session.exec(
+        select(Friend).where(Friend.owner_user_id == owner_user_id)
+    ).all()
+
+    # We return Friend objects; FastAPI will convert them to FriendRead
+    return friends
