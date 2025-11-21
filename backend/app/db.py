c3f70973d7115a3d897e13c2f8372eeabd4574fe
Elias Pamfilis <eliasp05@icloud.com>
2025-11-21 18:10:06 -0500

    slight changes to comments

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,17 +1,23 @@
 from sqlmodel import SQLModel, create_engine, Session
-from . import models
 from .models import IllnessLog, LogCreate
 
 DATABASE_URL = "sqlite:///./app.db"
 
-engine = create_engine(DATABASE_URL, echo=True, connect_args = {"check_same_thread":pip install fastapi "uvicorn[standard]" sqlmodel False})
+engine = create_engine(
+    DATABASE_URL,
+    echo=True,
+    connect_args={"check_same_thread": False}
+)
+
 
 def get_session():
     with Session(engine) as session:
         yield session
 
+
 def init_db():
     SQLModel.metadata.create_all(engine)
+
 
 def create_illness_log(session: Session, log_in: LogCreate) -> IllnessLog:
     db_log = IllnessLog.model_validate(log_in.model_dump())
